0db043b8016f2b1f40f9584eab3fcf013ff28e8a
Talbot_1033389 <1033389@my.scccd.edu>
2025-04-08 16:24:37 -0700

    implemented getImage and FindPath

--- mnt_path.cpp
+++ mnt_path.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <string>
 #include <limits>
+#include <cmath> 
 
 #include "DataSource.h"
 #include "data_src/ElevationData.h"
@@ -20,30 +21,80 @@ using namespace bridges;
 // takes in the processed elevation data and returns a color grid for
 // visualization using BRIDGES
 ColorGrid getImage(const ElevationData& elev_data) {
-  //TODO
-  //
-  //return a Color Grid that represents the elevation as gray scale
-  //
-  // access elevation with elev_data.getVal(row, col)
-  // compute the intensity by linear interpolation between elev_data.getMinVal(); and elev_data.getMaxVal();
-  //
-  // create ColorGrid with ColorGrid(nbrows, nbcols)
-  // fill ColorGrid with .set(row, col, color)
-  ColorGrid cg (1,1);
+  // Get the min and max elevation for given area
+  int min_elev = elev_data.getMinVal();
+  int max_elev = elev_data.getMaxVal();
+
+  // Get length and width of elev_data
+  int cols = elev_data.getCols();
+  int rows = elev_data.getRows();
   
+  // Create color Grid that has same width and length of elev_data and default color 0
+  ColorGrid cg(cols, rows, Color(0, 0, 0));
+
+  // Nested for loop to set color for each cell corisponding with elev_data value mapped to 0-255
+  for (int r = 0; r < rows; r++){
+  	for (int c = 0; c < cols; c++){
+		
+		// get the elevation for given row/col position
+		int elevation = elev_data.getVal(r, c);
+		// calculate elevation scaled to 0-255
+		int scaled = ((elevation - min_elev) * 255) / (max_elev - min_elev);
+
+		// set color at row/col position to scaled value
+		cg.set(r, c, Color(scaled, scaled, scaled));
+		}
+	}  
   return cg;
 }
 
 // determines the least effort path through the mountain starting a point on
 // the left edge of the image
 void findPath(const ElevationData&  elev_data, int startRow, ColorGrid& cg) {
-  //        Run the greedy path from (0, startRow) to the right of the image
-  //
-  // always move right, but select the right cell, the top right cell, or bottom right cell
-  // by minimizing the difference of elevation.
-  //
-  // Write path to the colorgrid
+  
+  int rows = cg.getHeight();
+  int cols = cg.getWidth();
+  int currentCol = 0;
+  int currentRow = startRow;
+  int upElev = 0;
+  int middleElev = 0;
+  int downElev = 0;
+  cg.set(startRow, 0, Color(255, 0, 0));
+  // Debug, print rows and cols
+  std::cout << "Rows: " << rows << ", Columns: " << cols << std::endl;
+  while (currentCol < cols - 1){
+    // this section gets the elevations of each possible next move
+    int currentElev = elev_data.getVal(currentRow, currentCol);
+	int middleElev = elev_data.getVal(currentRow,  (currentCol + 1));
+	// check that if current row is already at top of map
+	int upElev = elev_data.getVal((currentRow - 1), (currentCol + 1));
+	int downElev = elev_data.getVal((currentRow + 1), (currentCol + 1));
+	// This section calculates the differences between each possible spot and current spot
+	int middleDiff = abs(currentElev - middleElev);
+	int upDiff = abs(currentElev - upElev);
+	int downDiff = abs(currentElev - downElev); 
+
+    // Dang ol' if/else man
+	if ((middleDiff < upDiff) and (middleDiff < downDiff)){
+		currentRow = currentRow;
+	}
+	else if ((upDiff < middleDiff) and (upDiff < downDiff)){
+		currentRow -= 1;  
+	}
+	else if ((downDiff < middleDiff) and (downDiff < upDiff)){
+		currentRow +=1;
+	}
+    std::cout << "Step (Current Column): " << currentCol
+          << " | Current Elevation: " << currentElev
+          << " | Middle Elevation: " << middleElev
+          << " | Up Elevation: " << upElev
+          << " | Down Elevation: " << downElev << std::endl;
+
+	currentCol += 1;
+	cg.set(currentRow, currentCol, Color(255, 0, 0));
+	}
 }
+
 
 int main(int argc, char **argv) {
 
@@ -51,7 +102,7 @@ int main(int argc, char **argv) {
 
 
   // initialize Bridges
-  Bridges bridges(1, "JunKuang", "754592920126");
+  Bridges bridges(1, "tessjtalbot", "1729671476901");
 
   // defaults for row number and data file
   int startRow = 50;
